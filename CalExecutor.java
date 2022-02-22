4cebd637e05e8b462a522545ce9910c03d274694
SarahSakina <99794485+SarahSakina@users.noreply.github.com>
2022-02-22 15:14:03 +0530

    Add files via upload

---
+++ CalExecutor.java
@@ -0,0 +1,9 @@
+public class CalExecutor
+{
+public static void main(String args[])
+{
+Calculator c=new Calculator();
+System.out.println("20+10= "+c.add(20,10));
+System.out.println("20-10= "+c.sub(20,10));
+}
+}
